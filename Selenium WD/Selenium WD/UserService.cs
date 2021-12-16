d2fb346784093c6bbeff036b2a26d1f72f273570
Danya <danya.sydortsov@tech-stack.io>
2021-12-16 18:12:41 +0200

    added xpath signin on main page

--- Selenium WD/Selenium WD/UserService.cs
+++ Selenium WD/Selenium WD/UserService.cs
@@ -15,5 +15,12 @@ namespace Selenium_WD
         {
             this.driver = driver;
         }
+
+        private readonly By _signInButton = By.XPath("//*[@id='mui_user_login_row']");
+        private readonly By _registrationButton = By.XPath("//span[@class='j-wrap orange']");
+        private readonly By _nameInputButton = By.XPath("//input[@name='p_[NikName]']");
+        private readonly By _emailInputButton = By.XPath("//input[@name='p_[EMail]']");
+        private readonly By _passwordInputButton = By.XPath("//input[@name='p_[PW0]']");
+        private readonly By _acceptRegistrationButton = By.XPath("//button[@id='gxg47snjfol']");
     }
 }
