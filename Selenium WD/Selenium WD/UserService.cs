ecb39a15152ecdf593c8a12f8db5a3b634d11dd3
Danya <danya.sydortsov@tech-stack.io>
2021-12-16 16:51:09 +0200

    added webDriver and chromeDriver

---
+++ Selenium WD/Selenium WD/UserService.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_WD
+{
+    internal class UserService
+    {
+         private IWebDriver driver;
+
+        public UserService(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
